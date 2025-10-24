320e776904ca6b419758b46f7c9747ab5cdb7338
CamoDuck <cubebox52@gmail.com>
2025-10-23 19:27:05 -0700

    added comments

--- team-9-game/Scripts/Constants.cs
+++ team-9-game/Scripts/Constants.cs
@@ -1,6 +1,12 @@
 using Godot;
 using System;
 
+/// <summary>
+/// Holds project-wide constant values used across different systems and modules.
+/// </summary>
+/// <remarks>
+/// This class contains only static constant fields and should not be instantiated.
+/// </remarks>
 public static class Constants
 {
 }
--- team-9-game/Scripts/Utils.cs
+++ team-9-game/Scripts/Utils.cs
@@ -1,6 +1,12 @@
 using Godot;
 using System;
 
+/// <summary>
+/// Provides general-purpose helper and utility methods used throughout the project.
+/// </summary>
+/// <remarks>
+/// All methods in this class are static and can be called without creating an instance.
+/// </remarks>
 public static class Utils
 {
 }
