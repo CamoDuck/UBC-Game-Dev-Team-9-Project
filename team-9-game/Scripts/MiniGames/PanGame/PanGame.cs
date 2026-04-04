9bb0b94b050b6275ccc1de2cbfe193399948be8d
rxu53@student.ubc.ca <160555024+NOTAANAMEE@users.noreply.github.com>
2026-04-03 17:20:51 -0700

    Minigame: Pan Game

---
+++ team-9-game/Scripts/MiniGames/PanGame/PanGame.cs
@@ -0,0 +1,125 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public partial class PanGame : Node2D
+{
+	[Export] private AnimatableBody2D Pan { get; set; }
+
+	[Export] private float RotationCoefficient { get; set; } = 0.02f;
+
+	[Export] private float PanHeightRatio { get; set; } = 0.33f;
+
+	/// <summary>
+	/// A countdown timer. If you don't want to listen to OnGameFinishedEvent,
+	/// just leave it there.
+	/// </summary>
+	[Export] public float CountdownTime { get; set; } = float.NaN;
+	
+	/// <summary>
+	/// Event that triggers when fails or times up
+	/// </summary>
+	[Signal] public delegate void OnGameFinishedEventHandler();
+	
+	/// <summary>
+	/// If you want to pause the minigame, set it to true <br/>
+	/// Otherwise, set it to false
+	/// </summary>
+	public bool PauseGame { get; set; }
+	
+	private Vector2 MousePosition;
+	private float RotationSpeed;
+	private bool left;
+	private List<RigidBody2D> foods = [];
+	
+	private Vector2 Size => GetViewportRect().Size;
+
+	private Vector2 Center => Size / 2;
+
+	private Vector2 PanExpectedPosition 
+		=> new (Center.X, Size.Y * (1 - PanHeightRatio));
+	
+	private bool Left {
+		get => left;
+		set
+		{
+			if (left != value) RotationSpeed = 0;
+			left = value;
+		}
+	}
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		GD.Print("Size: " + Size);
+		GD.Print("Center: " + Center);
+		GD.Print("Pan Expected Pos: " + PanExpectedPosition);
+		
+	}
+	
+	/// <summary>
+	/// Add food to the list.
+	/// </summary>
+	/// <param name="food"></param>
+	public void AddFood(RigidBody2D food)
+	{
+		foods.Add(food);
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if (PauseGame) return;
+		Pan.SyncToPhysics = true;
+		MousePosition = GetGlobalMousePosition();
+		Left = MousePosition.X < Center.X;
+		RotationSpeed += (Left? -RotationCoefficient : RotationCoefficient) * (float)delta * 60;
+		CountdownTime -= (float)delta;
+		if (CountdownTime <= 0) EmitSignal(SignalName.OnGameFinished);
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		if (PauseGame) return;
+		base._PhysicsProcess(delta);
+		if (Pan.Position != PanExpectedPosition)
+		{
+			Pan.SyncToPhysics = false;
+			Pan.Position = PanExpectedPosition;
+			Pan.SyncToPhysics = true;
+		}
+			
+		switch (Pan.RotationDegrees)
+		{
+			case >= 90 when RotationSpeed > 0:
+				Pan.RotationDegrees = 90;
+				break;
+			case <= -90 when RotationSpeed < 0:
+				Pan.RotationDegrees = -90;
+				break;
+			default:
+				Pan.RotationDegrees += RotationSpeed * (float)delta * 150;
+				break;
+		}
+
+		if (!CheckNotFail()) EmitSignal(SignalName.OnGameFinished);
+	}
+
+	private bool CheckNotFail()
+	{
+		return foods.Count == 0 || foods.Any(CheckRigidBody);
+	}
+
+	private bool CheckRigidBody(RigidBody2D food)
+	{ 
+		var ret = food.Position is { X: >= 0 } &&
+		       food.Position.X <= Size.X && food.Position.Y <= Size.Y;
+		return ret;
+	}
+
+	public RigidBody2D[] GetAvailableFood()
+	{
+		return foods.Where(CheckRigidBody).ToArray();
+	}
+}
---
+++ team-9-game/Scripts/MiniGames/PanGame/PanGameExample.cs
@@ -0,0 +1,33 @@
+using Godot;
+using System;
+using System.Diagnostics;
+using System.Threading;
+
+public partial class PanGameExample : Node2D
+{
+	[Export] private PanGame PanGame { get; set; }
+	[Export] private RigidBody2D Food1 { get; set; }
+	[Export] private RigidBody2D Food2 { get; set; }
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		PanGame.CountdownTime = 5; // Five seconds count down
+		GD.Print(Food1);
+		PanGame.AddFood(Food1);
+		PanGame.AddFood(Food2);
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		
+	}
+
+	private void HandleGameFinished()
+	{
+		var availableRigid = PanGame.GetAvailableFood();
+		PanGame.PauseGame = true;
+		OS.Alert($"Game finished. {availableRigid.Length} left!");
+	}
+}
